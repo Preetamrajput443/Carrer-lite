462559f08d1c6efa40021fbd94c7c8867456fe09
preetamrajput443 / fungame <preetamrajput443@gmail.com>
2025-06-29 18:06:29 +0530

    Create Language.Context.jsx

---
+++ Src/Context/Language.Context.jsx
@@ -0,0 +1,19 @@
+import { createContext, useState, useContext } from 'react';
+
+const LanguageContext = createContext();
+
+export const LanguageProvider = ({ children }) => {
+  const [lang, setLang] = useState('en');
+
+  const toggleLang = () => {
+    setLang(prev => (prev === 'en' ? 'hi' : 'en'));
+  };
+
+  return (
+    <LanguageContext.Provider value={{ lang, toggleLang }}>
+      {children}
+    </LanguageContext.Provider>
+  );
+};
+
+export const useLanguage = () => useContext(LanguageContext);
