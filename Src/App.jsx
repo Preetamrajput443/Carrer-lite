ce5c728deedb3359eae0104e2091276f6b3104fa
preetamrajput443 / fungame <preetamrajput443@gmail.com>
2025-06-29 17:42:04 +0530

    Create App.jsx

---
+++ Src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Tools from './pages/Tools';
+import Customize from './pages/Customize';
+import MainLayout from './layouts/MainLayout';
+
+export default function App() {
+  return (
+    <Router>
+      <MainLayout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/tools" element={<Tools />} />
+          <Route path="/customize" element={<Customize />} />
+          <Route path="*" element={<h1 className="text-center text-2xl mt-10">404 - Page Not Found</h1>} />
+        </Routes>
+      </MainLayout>
+    </Router>
+  );
+}
