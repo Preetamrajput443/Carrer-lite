d2ac1a4e3eeddfd99691c45e5dd96cdc81d6ea91
preetamrajput443 / fungame <preetamrajput443@gmail.com>
2025-06-30 20:36:43 +0530

    Update App.jsx

--- Src/App.jsx
+++ Src/App.jsx
@@ -1,20 +1,9 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Tools from './pages/Tools';
-import Customize from './pages/Customize';
-import MainLayout from './layouts/MainLayout';
-
-export default function App() {
+function App() {
   return (
-    <Router>
-      <MainLayout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/tools" element={<Tools />} />
-          <Route path="/customize" element={<Customize />} />
-          <Route path="*" element={<h1 className="text-center text-2xl mt-10">404 - Page Not Found</h1>} />
-        </Routes>
-      </MainLayout>
-    </Router>
-  );
+    <div className="text-center text-xl text-blue-600 mt-10">
+      Career Lite is working! 🚀
+    </div>
+  )
 }
+
+export default App
