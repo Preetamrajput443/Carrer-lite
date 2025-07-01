081245dbe23993e27e26a5a94f0b67cfbfb02cfd
preetamrajput443 / fungame <preetamrajput443@gmail.com>
2025-07-01 17:53:00 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
-function App() {
-  return (
-    <div className="text-center text-xl text-blue-600 mt-10">
-      Career Lite is working! 🚀
-    </div>
-  )
-}
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Tools from './pages/Tools';
+import Customize from './pages/Customize';
+import MainLayout from './layouts/MainLayout';
 
-export default App
+export default function App() {
+  return (
+    <Router>
+      <MainLayout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/tools" element={<Tools />} />
+          <Route path="/customize" element={<Customize />} />
+          <Route
+            path="*"
+            element={
+              <h1 className="text-center text-2xl mt-10">
+                404 - Page Not Found
+              </h1>
+            }
+          />
+        </Routes>
+      </MainLayout>
+    </Router>
+  );
+}
